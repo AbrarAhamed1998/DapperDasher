a634bfc8973fd24b21ebea06f06878fe16e34b4b
AbrarAhamed1998 <abrarahamed1998@gmail.com>
2024-09-24 23:35:54 +0530

    Added sprites

--- dapper.cpp
+++ dapper.cpp
@@ -1,29 +1,56 @@
 #include "raylib.h"
 int main()
 {
+    #pragma region CONST FIELDS
+
     const int windowHeight = 300;
     const int windowWidth = 600;
-    
-    InitWindow(windowWidth, windowHeight, "DapperDasher");
+    const char windowName[] = "DapperDasher";
+    const char scarfySpritePath[] = "textures/scarfy.png";
+    const int gravity = 1000;
 
-    const int gravity = 1;
+    #pragma endregion CONST FIELDS
 
-    int rectWidth = 30;
-    int rectHeight = 50;
+    #pragma region PRIVATE FIELDS
+
+    // int rectWidth = 30;
+    // int rectHeight = 50;
     int groundVal = windowHeight;
-    int posY = windowHeight/2;
+    //int posY = windowHeight/2;
     int velocityY = 0;
-    int jumpForce = 20;
+    int jumpForce = 600;
     
     bool isGrounded;
 
+    #pragma endregion PRIVATE FIELDS
+
+    #pragma region GAME START
+
+    InitWindow(windowWidth, windowHeight, windowName);
     SetTargetFPS(60);
+    Texture2D scarfySprite = LoadTexture(scarfySpritePath);
+    Rectangle scarfyRect;
+    scarfyRect.width = scarfySprite.width/6;
+    scarfyRect.height = scarfySprite.height;
+    scarfyRect.x = 0;
+    scarfyRect.y = 0;
+
+    Vector2 scarfyPos;
+    scarfyPos.x = windowWidth/2 - scarfyRect.width/2;
+    scarfyPos.y = windowHeight - scarfyRect.height;
+
+    #pragma endregion GAME START
+
+    #pragma region GAME LOOP
+
     while(!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
 
-        if(posY >= (groundVal-rectHeight))
+        float deltaTime = GetFrameTime();
+
+        if(scarfyPos.y >= (groundVal-scarfyRect.height))
         {
             isGrounded = true;
             velocityY = 0;
@@ -35,7 +62,7 @@ int main()
 
         if(!isGrounded)
         {
-            velocityY += gravity;
+            velocityY += gravity * deltaTime;
         }
 
         if(IsKeyPressed(KEY_SPACE) && isGrounded)
@@ -43,12 +70,22 @@ int main()
             velocityY -= jumpForce; 
         }
 
-        posY += velocityY;
+        scarfyPos.y += velocityY * deltaTime;
         
+        DrawTextureRec(scarfySprite, scarfyRect, scarfyPos, WHITE);
         
-        DrawRectangle(windowWidth/2, posY, rectWidth, rectHeight, BLUE);
+        //DrawRectangle(windowWidth/2, posY, rectWidth, rectHeight, BLUE);
 
         EndDrawing();
     }
+    
+    #pragma endregion GAME LOOP
+
+    #pragma region GAME END 
+
+    UnloadTexture(scarfySprite);
     CloseWindow();
+
+    #pragma endregion GAME END
+
 }
